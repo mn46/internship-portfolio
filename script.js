4f55940055fa1652fb9ea7c6ad263c08e93fee6d
Marta <m.nowak033@gmail.com>
2022-10-29 21:47:31 +0200

    card templates work

--- script.js
+++ script.js
@@ -13,3 +13,31 @@
 //     "github-link": "https://github.com/mn46/hogwart-student-list",
 //     "project-link": "https://mn46.github.io/hogwart-student-list/"
 // },
+
+window.addEventListener("DOMContentLoaded", start);
+
+const url = "projects.json";
+
+function start() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      console.log(jsonData);
+      jsonData.project.forEach(addContent);
+    });
+}
+
+function addContent(project) {
+  const cardTemplate = document.querySelector("#project-card-template").content;
+  const imageTemplate = document.querySelector("#carousel-picture").content;
+
+  const cardClone = cardTemplate.cloneNode(true);
+  const imageClone = imageTemplate.cloneNode(true);
+
+  cardClone.querySelector("img").src = project.pictureCard;
+  cardClone.querySelector("h3").textContent = project.projectName;
+  cardClone.querySelector("p").textContent = project.descriptionShort;
+  cardClone.querySelector("button").dataset.projectnick = project.buttonInfo;
+
+  document.querySelector("#projects_container").appendChild(cardClone);
+}
