4a1cbcf03419a118fd462097f4f7b3ede92bcbec
Marta <m.nowak033@gmail.com>
2022-10-29 19:20:15 +0200

    Added json file, started adding projects

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 "use strict";
 
-import { animate, scroll, stagger, inView } from "https://cdn.skypack.dev/motion";
+// motionOne  link
+// import { animate, scroll, stagger, inView } from "https://cdn.skypack.dev/motion";
+
+// JSON object here, because I can't comment in JSON............
+// {
+//     "project-name": "Learning Bootstrap",
+//     "description-short": "A simple website created using mostly Bootstrap.",
+//     "description-long": "The goal of the project was to learn basics of Bootstrap and try to make a website using it as much as possible. Thanks to that I had a chance to explore its capabilities.",
+//     "picture-card": "",
+//     "picture-content": "",
+//     "github-link": "https://github.com/mn46/hogwart-student-list",
+//     "project-link": "https://mn46.github.io/hogwart-student-list/"
+// },
