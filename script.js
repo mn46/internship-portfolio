5f1bc9a79ddb5591ba5620b9732399ec41c33c44
Marta <m.nowak033@gmail.com>
2022-10-30 12:47:09 +0100

    slideshow works

--- script.js
+++ script.js
@@ -23,21 +23,52 @@ function start() {
     .then((response) => response.json())
     .then((jsonData) => {
       console.log(jsonData);
-      jsonData.project.forEach(addContent);
+      jsonData.projects.forEach(addContent);
     });
 }
 
 function addContent(project) {
   const cardTemplate = document.querySelector("#project-card-template").content;
-  const imageTemplate = document.querySelector("#carousel-picture").content;
 
   const cardClone = cardTemplate.cloneNode(true);
-  const imageClone = imageTemplate.cloneNode(true);
 
+  //   adding content to cards on home page
   cardClone.querySelector("img").src = project.pictureCard;
   cardClone.querySelector("h3").textContent = project.projectName;
   cardClone.querySelector("p").textContent = project.descriptionShort;
-  cardClone.querySelector("button").dataset.projectnick = project.buttonInfo;
+  cardClone.querySelector("button").dataset.projnick = project.buttonInfo;
+  console.log(project);
+  //   event listener on button so that there's correct information in the modal
+  cardClone.querySelector(".card_button").addEventListener("click", () => populateModal(project));
+
+  // appending clone to the parent
 
   document.querySelector("#projects_container").appendChild(cardClone);
+}
+
+function populateModal(project) {
+  console.log(project);
+  // adding data to modal
+
+  document.querySelector("#project h2").textContent = project.projectName;
+  document.querySelector(".project_description").textContent = project.descriptionLong;
+  document.querySelector("a.github").textContent = project.githubLink;
+  document.querySelector("a.project-link").textContent = project.projectLink;
+
+  // creating image carousel
+
+  project.pictureModal.forEach((picture) => {
+    console.log(picture);
+    const imageTemplate = document.querySelector("#carousel-picture").content;
+    const imageClone = imageTemplate.cloneNode(true);
+
+    imageClone.querySelector("img").src = picture;
+
+    document.querySelector(".carousel-inner").appendChild(imageClone);
+
+    console.log("added picture");
+  });
+
+  console.log("selecting carousel item");
+  document.querySelector(".carousel-item:nth-child(2)").classList.add("active");
 }
